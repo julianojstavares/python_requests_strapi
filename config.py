f472224cc3222ba1e365aba0835db10ccfbc5fe7
Juliano <julianojstavares@outlook.com>
2021-02-03 09:26:55 -0300

    New url to test in config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
-api_url = 'https://strapi-platform.herokuapp.com'
+# api_url = 'https://strapi-platform.herokuapp.com'
+api_url = 'http://144.91.71.134:1337'
 end_point = 'clientes'
 content_type = {'Content-Type': 'application/json'}
